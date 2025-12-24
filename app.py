eb00aeedff7312dfb059c3bb610cdb814a5dc41e
Pritam Ghosh <157035723+Monu9500@users.noreply.github.com>
2025-12-24 19:09:17 +0530

    Reformat app.py with consistent import structure

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
-credit_score = st.number_input('Credit Score')g
+credit_score = st.number_input('Credit Score')
 estimated_salary = st.number_input('Estimated Salary')
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
@@ -68,3 +68,4 @@ if prediction_proba > 0.5:
     st.write('The customer is likely to churn.')
 else:
     st.write('The customer is not likely to churn.')
+
